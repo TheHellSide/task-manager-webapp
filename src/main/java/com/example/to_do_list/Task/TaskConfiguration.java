c64b0b5582ba5be667cedad0eb5479c18b414e63
TheHellSide <ninja.fab.03@gmail.com>
2025-08-06 15:54:18 +0200

    Added the example task and the thread Oreder, FIXED CONCURRENCY

--- src/main/java/com/example/to_do_list/Task/TaskConfiguration.java
+++ src/main/java/com/example/to_do_list/Task/TaskConfiguration.java
@@ -5,30 +5,33 @@ import com.example.to_do_list.User.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import java.time.LocalDate;
 import java.util.List;
 
 @Configuration
 public class TaskConfiguration {
-//    @Bean
-//    CommandLineRunner taskCommandLineRunner(TaskRepository taskRepository, UserRepository userRepository) {
-//        return args -> {
-//            User joe = userRepository.findUserByEmail("joe@admin.com")
-//                    .orElseThrow(() -> new IllegalStateException("User not found"));
-//
-//            Task new_Task = new Task(
-//                    "New task",
-//                    "Descrizione del task",
-//                    LocalDate.now(),
-//                    TaskPriority.HIGH,
-//                    joe
-//            );
-//
-//            for (Task task : List.of(new_Task)) {
-//                if (!taskRepository.findAll().contains(task)){
-//                    taskRepository.save(task);
-//                }
-//            }
-//        };
-//    }
+    @Bean
+    @Order(2)
+    CommandLineRunner taskCommandLineRunner(TaskRepository taskRepository, UserRepository userRepository) {
+        return args -> {
+            User anonymous = userRepository.findUserByEmail("anonymous@admin.com")
+                    .orElseThrow(() -> new IllegalStateException("User not found"));
+
+            Task new_Task = new Task(
+                    "New task",
+                    "Task descriptio. This is an example of a simple task.",
+                    LocalDate.now(),
+                    TaskPriority.HIGH,
+                    anonymous
+            );
+
+
+            for (Task task : List.of(new_Task)) {
+                if (!taskRepository.findAll().contains(task)){
+                    taskRepository.save(task);
+                }
+            }
+        };
+    }
 }
--- src/main/java/com/example/to_do_list/User/UserConfiguration.java
+++ src/main/java/com/example/to_do_list/User/UserConfiguration.java
@@ -3,23 +3,25 @@ package com.example.to_do_list.User;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import java.util.List;
 import java.util.Optional;
 
 @Configuration
 public class UserConfiguration {
     @Bean
+    @Order(1)
     CommandLineRunner userCommandLineRunner (UserRepository userRepository){
         return args -> {
             User admin = new User(
                     "admin@admin.com",
-                    "Admin",
+                    "admin",
                     "admin123"
             );
 
             User anonymous = new User(
                     "anonymous@admin.com",
-                    "Anonymous",
+                    "anonymous",
                     ""
             );
 
